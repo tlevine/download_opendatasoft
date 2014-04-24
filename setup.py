bab831d0dd3bdfc178966f9601b15d65a30b1aef
Thomas Levine <_@thomaslevine.com>
2014-04-24 15:06:14 +0000

    setup.py

---
+++ setup.py
@@ -0,0 +1,16 @@
+from distutils.core import setup
+
+from download_opendatasoft import __version__
+
+setup(name='download_opendatasoft',
+      author='Thomas Levine',
+      author_email='_@thomaslevine.com',
+      description='Download data from OpenDataSoft open data platforms.',
+      url='https://github.com/tlevine/download_opendatasoft',
+      packages=['download_opendatasoft'],
+      install_requires = ['picklecache'],
+      extras_require = [],
+      tests_require = ['nose'],
+      version=__version__,
+      license='AGPL',
+)
