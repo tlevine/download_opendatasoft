7640e2d64b6b40a77fc22ff231f51bc5b8b6fc5d
Thomas Levine <_@thomaslevine.com>
2014-04-24 15:14:10 +0000

    copy a test

---
+++ download_opendatasoft/test/test_download.py
@@ -0,0 +1,17 @@
+from collections import namedtuple
+import json
+
+import nose.tools as n
+
+import download_opendatasoft.download
+
+Response = namedtuple('Response', ['text','ok'])
+
+def test_datasets():
+    def get(url):
+        return Response(text = '{"datasets":[{"foo":8}]}', ok = True)
+    catalog = 'stnoheustahoe'
+
+    observed = download_opendatasoft.download.datasets(get, catalog)
+    expected = [{'foo':8,'catalog':catalog}]
+    n.assert_list_equal(observed, expected)
